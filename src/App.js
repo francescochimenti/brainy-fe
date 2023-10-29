88e47cea25b5c6445d2ff4d51e962afe36a43db5
Francesco Chimenti <francescochimenti99@icloud.com>
2023-10-29 15:19:05 +0100

    setting-folder-and-tailwind

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div>
-      <h1>Hi there!</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
