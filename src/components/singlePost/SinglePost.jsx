b9c5c122f5dde410f0267a4d606f916b07c8ee87
Francesco Chimenti <francescochimenti99@icloud.com>
2023-11-12 14:36:52 +0100

    Refactor SinglePost component to handle different post author roles

--- src/components/singlePost/SinglePost.jsx
+++ src/components/singlePost/SinglePost.jsx
@@ -29,44 +29,87 @@ const SinglePost = ({ post }) => {
       }
     };
   }, []);
-  return (
-    <div
-      ref={postRef}
-      className={`my-4 max-w-4xl rounded-lg  bg-white px-10 py-6 text-gray-700 shadow-md dark:bg-black dark:text-white dark:shadow-sm dark:shadow-white md:w-[800px] ${
-        isVisible ? "animate__animated animate__bounceInLeft" : ""
-      }`}
-    >
-      <div className="flex items-center justify-between">
-        <BrainyLogo />
-        <LikeButton post={post} />
-      </div>
-      <div className="mt-2 font-semibold">
-        <p className="mt-2 ">"{post.content}"</p>
-      </div>
-      <div className="mt-4 flex items-center justify-between">
-        <span className="mr-4 text-slate-500 md:mr-0">
-          {new Date(post.createdAt).toLocaleDateString()}
-        </span>
-        <div>
-          <div className="flex items-center">
-            <Link to={`/profile/${post.author._id}`}>
-              <img
-                className="mr-2 h-10 min-h-[40px] w-10 min-w-[40px] rounded-full object-cover "
-                src={post.author.avatar}
-                alt="avatar"
-              />
-            </Link>
-
-            <span className="font-semibold">
+  if (post.author.role !== "Company")
+    return (
+      <div
+        ref={postRef}
+        className={`my-4 max-w-4xl rounded-lg  bg-white px-10 py-6 text-gray-700 shadow-md dark:bg-black dark:text-white dark:shadow-sm dark:shadow-white md:w-[800px] ${
+          isVisible ? "animate__animated animate__bounceInLeft" : ""
+        }`}
+      >
+        <div className="flex items-center justify-between">
+          <BrainyLogo />
+          <LikeButton post={post} />
+        </div>
+        <div className="mt-2 font-semibold">
+          <p className="mt-2 ">"{post.content}"</p>
+        </div>
+        <div className="mt-4 flex items-center justify-between">
+          <span className="mr-4 text-slate-500 md:mr-0">
+            {new Date(post.createdAt).toLocaleDateString()}
+          </span>
+          <div>
+            <div className="flex items-center">
               <Link to={`/profile/${post.author._id}`}>
-                {post.author.firstName} {post.author.lastName}
+                <img
+                  className="mr-2 h-10 min-h-[40px] w-10 min-w-[40px] rounded-full object-cover "
+                  src={post.author.avatar}
+                  alt="avatar"
+                />
               </Link>
-            </span>
+
+              <span className="font-semibold">
+                <Link to={`/profile/${post.author._id}`}>
+                  {post.author.firstName} {post.author.lastName}
+                </Link>
+              </span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+
+  if (post.author.role === "Company")
+    return (
+      <div
+        ref={postRef}
+        className={`my-4 max-w-4xl rounded-lg  bg-white px-10 py-6 text-gray-700 shadow-md dark:bg-black dark:text-white dark:shadow-sm dark:shadow-white md:w-[800px] ${
+          isVisible ? "animate__animated animate__bounceInLeft" : ""
+        }`}
+      >
+        <div className="flex items-center justify-between">
+          <BrainyLogo />
+          <p className="rounded bg-orange-400 px-2 py-1 font-bold text-white dark:bg-orange-500 dark:text-white">
+            ADS
+          </p>
+        </div>
+        <div className="mt-2 font-semibold">
+          <p className="mt-2 ">"{post.content}"</p>
+        </div>
+        <div className="mt-4 flex items-center justify-between">
+          <span className="mr-4 text-slate-500 md:mr-0">
+            {new Date(post.createdAt).toLocaleDateString()}
+          </span>
+          <div>
+            <div className="flex items-center">
+              <Link to={`/profile/${post.author._id}`}>
+                <img
+                  className="mr-2 h-10 min-h-[40px] w-10 min-w-[40px] rounded-full object-cover "
+                  src={post.author.avatar}
+                  alt="avatar"
+                />
+              </Link>
+
+              <span className="font-semibold">
+                <Link to={`/profile/${post.author._id}`}>
+                  {post.author.firstName}
+                </Link>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
 };
 
 export default SinglePost;
