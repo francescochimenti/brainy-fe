6376fcbbbd0cb159e98c3057f7835e05f4d824b9
Francesco Chimenti <francescochimenti99@icloud.com>
2023-11-17 21:33:47 +0100

    v.1

--- src/pages/newAuth/NewAuth.jsx
+++ src/pages/newAuth/NewAuth.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import Register from "../../components/register/Register";
-import Footer from "../../components/footer/Footer";
 
 const NewAuth = () => {
   return (
     <>
       <Register />
-      <Footer />
     </>
   );
 };
